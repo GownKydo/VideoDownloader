25ac7035ef6cd70daa47f8f9361e9338fc75f682
corniergown <gerardohgomez0112@gmail.com>
2024-07-27 18:42:05 -0500

    add options for download videos

--- src/main.py
+++ src/main.py
@@ -1,14 +1,31 @@
-from packages import DownloadVideo
-from packages import DownloadPlaylist
-from packages import DownloadAudio
+from packages.youtube import DownloadAudio, DownloadPlaylist, DownloadVideo
 
 import os
+from time import sleep
 
 def menu():
-    print("\n1. Download Video")
-    print("2. Download Playlist")
-    print("3. Download Only Audio")
-    print("4. Exit\n")
+    print("""
+    ##########################################
+    #####                                #####
+    #####   Tools for Download Videos    #####
+    #####                                #####
+    ##########################################
+
+  -_-_-_-_-_--_-_-_-_-_--_-_-_-_-_--_-_-_-_-_-_
+
+  [1] Youtube\t[2] Twitteer/X\t[3] Instagram
+
+  [0] Exit
+   \n""")
+
+def second_menu():
+    print("""\n
+    1. Download Video.
+    2. Download Only Audio.
+    3. Download Playlist.
+    4. Exit.
+    """)
+
 
 def main():
     while True:
@@ -22,25 +39,42 @@ def main():
         except ValueError:
             print("\nt[!] Please enter a valid number!")
             continue
-
-        if choice == 1:
+        
+        if (choice == 1):
+            second_menu()
+            option = int(input("[*] Enter your choise: "))
+            
+            if (option == 1):
+                url = input("[*] Enter the video URL: ")
+                print()
+                DownloadVideo.download_video(url)
+                print()
+            elif (option == 2):
+                url = input("[*] Enter the video URL: ")
+                print()
+                DownloadAudio.download_audio(url)
+                print()
+            elif (option == 3):
+                url = input("[*] Enter the video URL: ")
+                print()
+                DownloadPlaylist.download_playlist(url)
+                print()
+            else:
+                print("Enter a Valid Option!\n")
+        
+        if (choice == 2):
             url = input("[*] Enter the video URL: ")
             DownloadVideo.download_video(url)
-            os.system('cls' if os.name == 'nt' else 'clear')
+            print()
+        
+        if (choice == 3):
+            print("Building . . ")
+            sleep(3)
 
-        elif choice == 2:
-            url = input("[*] Enter the playlist URL: ")
-            DownloadPlaylist.download_playlist(url)
-
-        elif choice == 3:
-            url = input("[*] Enter the audio URL: ")
-            DownloadAudio.download_audio(url)
-
-        elif choice == 4:
-            print("\t[!] Goodbye!")
-            break
-        else:
-            print("\n\t[!] Invalid choice!")
+        if (choice == 4):
+            print("\n\tGoog Bye! :)\n")
+            sleep(3)
+            break;
 
 if __name__ == '__main__':
     main()
